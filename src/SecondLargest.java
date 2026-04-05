ae827ed1743eaa280d054433f9d89a4969fccfaf
Aswin M Kumar <aswin2k03.mk@gmail.com>
2026-04-05 20:42:58 +0530

    Fix Experiment 12: Correctly handle duplicate maximum elements

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -17,7 +17,7 @@ public class SecondLargest {
                     if (arr[i] > max1) {
                         max2 = max1;
                         max1 = arr[i];
-                    } else if (arr[i] > max2 && arr[i] != max1) {
+                    } else if (arr[i] > max2) {
                         max2 = arr[i];
                     }
                 }
